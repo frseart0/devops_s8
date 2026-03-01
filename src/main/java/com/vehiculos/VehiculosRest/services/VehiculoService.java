8d3ca02255e9431614b8b440ab04f6d42c8ccf5d
frsearto <frsearto@gmail.com>
2026-03-01 02:47:34 -0300

    Update VehiculoService.java

--- src/main/java/com/vehiculos/VehiculosRest/services/VehiculoService.java
+++ src/main/java/com/vehiculos/VehiculosRest/services/VehiculoService.java
@@ -13,7 +13,7 @@ import org.springframework.stereotype.Service;
 
 /**
  *
- * @author Villacura AAAAAAAAAA
+ * @author Villacura AAAAAAAAAAASDASDASDAS
  */
 @Service
 public class VehiculoService {
