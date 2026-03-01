8c09ad2a2a5fd6a4e4d8349fa41d406cab96a63e
frsearto <frsearto@gmail.com>
2026-03-01 12:21:25 -0300

    commit

--- src/main/java/com/vehiculos/VehiculosRest/VehiculosRestApplication.java
+++ src/main/java/com/vehiculos/VehiculosRest/VehiculosRestApplication.java
@@ -2,11 +2,18 @@ package com.vehiculos.VehiculosRest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class VehiculosRestApplication {
+public class VehiculosRestApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(VehiculosRestApplication.class, args);
-	}
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(VehiculosRestApplication.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(VehiculosRestApplication.class, args);
+    }
 }
